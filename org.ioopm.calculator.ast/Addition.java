01dfca41aebbc894e7ac404df658ed49b8b0ae12
Jonathan Franzen <jonathan.franzeen@gmail.com>
2018-11-14 09:50:24 +0100

    addtioton and constant

---
+++ org.ioopm.calculator.ast/Addition.java
@@ -0,0 +1,6 @@
+package org.ioopm.calculator.ast;
+
+public class Addition extends Binary {
+	private SymbolicExpression lhs;
+	private SymbolicExpression rhs;
+}
---
+++ org.ioopm.calculator.ast/Constant.java
@@ -0,0 +1,9 @@
+package org.ioopm.calculator.ast;
+
+public class Constant {
+	private double value;
+	
+	public Constant(double x) {
+		value = x;
+	}
+}
