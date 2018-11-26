c7106c61b5f3cc589a1b0b95cb1a46e2041d773a
Hampus Widén <hampus.widen@hotmail.com>
2018-11-26 14:16:39 +0100

    parser leggo

--- org/ioopm/calculator/parser/CalculatorParser.java
+++ org/ioopm/calculator/parser/CalculatorParser.java
@@ -16,7 +16,7 @@ public class CalculatorParser {
 	}
 	
 	public void parse(String inputString) {
-		this.st = new StreamTokenizer(new StringReader("vars"));
+		this.st = new StreamTokenizer(new StringReader("45 + 10"));
 	}
 
 	public SymbolicExpression statement() throws IOException {
@@ -39,14 +39,134 @@ public class CalculatorParser {
 				return new Constant(1);
 			}
 		} else {
-			return this.addition();
+			return assignment();
 		}
 		return new Constant(1337);
 	}
 
-	public SymbolicExpression addition() throws IOException {
-		return new Constant(this.st.nval);
+	public SymbolicExpression assignment() throws IOException {
+		// Calculate AST for LHS and assign to RHS if RHS exists!
+		
+		//LHS
+		SymbolicExpression result = expression();
+
+		//RHS
+		this.st.nextToken();
+		while (this.st.ttype == '=') {
+			this.st.nextToken();
+			SymbolicExpression identifier = new Variable(this.st.sval);
+			result = new Assignment(result, identifier);
+		}
+		this.st.pushBack();
+
+		return result;
 	}
+
+	public SymbolicExpression expression() throws IOException {
+		/// Read a term and make it the current sum 
+        SymbolicExpression lhs = term();
+        /// Read the next token and put it in sval/nval/ttype depending on the token
+        this.st.nextToken();
+        /// If the token read was + or -, go into the loop 
+        while (this.st.ttype == '+' || this.st.ttype == '-') {
+        	SymbolicExpression rhs = term();
+            if(this.st.ttype == '+'){
+                /// If we are adding things, read a term and add it to the current sum
+                lhs = new Addition(lhs,rhs);
+            } else {
+                /// If we are adding things, read a term and subtract it from the current sum
+                lhs = new Subtraction(lhs,rhs);
+            }
+            /// Read the next token into sval/nval/ttype so we can go back in the loop again
+            this.st.nextToken();
+        }
+        /// If we came here, we read something which was not a + or -, so we need to put
+        /// that back again (whatever it was) so that we did not accidentally ate it up!
+        this.st.pushBack();
+        /// We are done, return sum
+        return lhs;
+    }
+
+    public SymbolicExpression term() throws IOException {
+		/// Read a term and make it the current sum 
+        SymbolicExpression lhs = primary();
+        /// Read the next token and put it in sval/nval/ttype depending on the token
+        this.st.nextToken();
+        /// If the token read was + or -, go into the loop 
+        while (this.st.ttype == '*' || this.st.ttype == '/') {
+        	SymbolicExpression rhs = primary();
+            if(this.st.ttype == '*'){
+                /// If we are adding things, read a term and add it to the current sum
+                lhs = new Multiplication(lhs,rhs);
+            } else {
+                /// If we are adding things, read a term and subtract it from the current sum
+                lhs = new Division(lhs,rhs);
+            }
+            /// Read the next token into sval/nval/ttype so we can go back in the loop again
+            this.st.nextToken();
+        }
+        /// If we came here, we read something which was not a + or -, so we need to put
+        /// that back again (whatever it was) so that we did not accidentally ate it up!
+        this.st.pushBack();
+        /// We are done, return sum
+        return lhs;
+    }
+
+    public SymbolicExpression primary() throws IOException {
+    	this.st.nextToken();
+    	SymbolicExpression result;
+
+    	if (this.st.ttype == '(') {
+    		result = assignment();
+
+    		if (this.st.nextToken() != ')') {
+                throw new SyntaxErrorException("expected ')'");
+            }
+
+    	} else if (this.st.ttype == this.st.TT_WORD) {
+    		if (this.st.sval.equals("exp") || this.st.sval.equals("log") || this.st.sval.equals("sin") || this.st.sval.equals("cos")) {
+    			result = unary();
+    		} else { //Variable
+    			result = identifier();
+    		}
+    	} else if (this.st.ttype == '-') {
+    		result = unary();
+    	} else {
+    		result = number();
+    	}
+    	return result;
+    }
+
+    public SymbolicExpression unary() throws IOException {
+    	SymbolicExpression result = new Constant(1337);
+    	this.st.nextToken();
+		SymbolicExpression primary = primary();
+		this.st.pushBack();
+		if (this.st.ttype == this.st.TT_WORD) {
+			if (this.st.sval.equals("exp")) {
+				result = new Exp(primary);
+			} else if (this.st.sval.equals("log")) {
+				result = new Log(primary);
+			} else if (this.st.sval.equals("sin")) {
+				result = new Sin(primary);
+			} else if ((this.st.sval.equals("cos"))) {
+				result = new Cos(primary);
+			}
+		} else { // Negation
+			result = new Negation(primary);
+		}
+		this.st.nextToken();
+		return result;
+    }
+
+    public SymbolicExpression identifier() throws IOException {
+    	return new Variable(this.st.sval);
+    }
+
+    public SymbolicExpression number() throws IOException {
+    	return new Constant(this.st.nval);
+    }
+
 
 	public static void main(String[] args) {
         CalculatorParser p = new CalculatorParser();
